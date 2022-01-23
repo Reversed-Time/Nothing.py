9aeacf21538e09db023fc4fc041fb27cf0a718fa
Reversed-Time <94551588+Reversed-Time@users.noreply.github.com>
2022-01-23 12:14:28 +0100

    Add files via upload

---
+++ Nothing.py
@@ -0,0 +1,6 @@
+from tkinter import*
+window = Tk()
+window.title("nothing.")
+label = Label(window, text =  'yes, there is nothing here \n now you can get out. \n \n By the way, this was made by Reversed-Time.')
+label.pack (padx = 80, pady = 30)
+window.mainloop()
